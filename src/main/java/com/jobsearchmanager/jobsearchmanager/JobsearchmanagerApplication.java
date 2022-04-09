72b5a949221833ea70748117ff419208a13d7f8e
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2022-04-09 21:35:32 +0200

    Initial commit

---
+++ src/main/java/com/jobsearchmanager/jobsearchmanager/JobsearchmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.jobsearchmanager.jobsearchmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsearchmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsearchmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobsearchmanager/jobsearchmanager/JobsearchmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobsearchmanager.jobsearchmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsearchmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
